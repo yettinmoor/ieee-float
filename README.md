1c95d1f9406ceeba61326c681628638c3b2960d7
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2020-08-29 13:42:58 +0200

    refactored and added f64

--- README.md
+++ README.md
@@ -1,43 +1,84 @@
-# IEEE-754 32-bit float analyzer
+# IEEE-754 32/64-bit float analyzer
 
-This very simple program breaks down float32 values into sign bit, exponent and mantissa. Written as an exercise to learn more about IEEE-754 floats.
+This very simple program breaks down float32 and float64 values into sign bit, exponent and mantissa. Written as an exercise to learn more about IEEE-754 floats.
 
 
 ```zig
 pub fn main() !void {
-    analyzeFloat(8);
-    analyzeFloat(9.1);
-    analyzeFloat(-120.42);
-    analyzeFloat(@bitCast(f32, @as(u32, 0x7f80_0000))); // inf
+    analyzeFloat(f32, 8);
+    analyzeFloat(f32, 9.1);
+    analyzeFloat(f32, 1.23e-40);
+    analyzeFloat(f32, -120.42);
+    analyzeFloat(f32, @bitCast(f32, @as(u32, 0x7f80_0000))); // inf
+
+    analyzeFloat(f64, 16);
+    analyzeFloat(f64, 12345678900000000000000000.1);
+    analyzeFloat(f64, 1e-310);
+    analyzeFloat(f64, @bitCast(f64, @as(u64, 0x7ff0_0000_0000_0001))); // NaN
 }
 ```
 
 ```
-value    | 8.0000e+00
+value    | f32: 8.00000 (8.00000e+00)
 -----------------------
 bits     | 0x41000000
 sign     | 0
-mantissa | 1.00000e+00 (0x000000)
-exp      | 3 (130)
+mantissa | 1.00000000 (0x000000)
+exponent | 3
 
-value    | 9.1000e+00
+value    | f32: 9.10000 (9.10000e+00)
 -----------------------
 bits     | 0x4111999a
 sign     | 0
-mantissa | 1.13750e+00 (0x11999a)
-exp      | 3 (130)
+mantissa | 1.13750005 (0x11999a)
+exponent | 3
 
-value    | -1.2042e+02
+value    | f32: 0.00000 (1.23000e-40)
+-----------------------
+bits     | 0x000156e0
+sign     | 0
+mantissa | 0.01046371 (0x0156e0)
+exponent | -126
+
+value    | f32: -120.42000 (-1.20420e+02)
 -----------------------
 bits     | 0xc2f0d70a
 sign     | 1
-mantissa | -1.88156e+00 (0x70d70a)
-exp      | 6 (133)
+mantissa | -1.88156247 (0x70d70a)
+exponent | 6
 
-value    | inf
+value    | f32: inf (inf)
 -----------------------
 bits     | 0x7f800000
 sign     | 0
-mantissa | 1.00000e+00 (0x000000)
-exp      | 255 (special value)
+mantissa | 1.00000000 (0x000000)
+exponent | 255 (special)
+
+value    | f64: 16.00000 (1.60000e+01)
+-----------------------
+bits     | 0x4030000000000000
+sign     | 0
+mantissa | 1.00000000 (0x000000)
+exponent | 4
+
+value    | f64: 12345678900000000000000000.00000 (1.23457e+25)
+-----------------------
+bits     | 0x45246c99303c37aa
+sign     | 0
+mantissa | 1.13825664 (0x46c99303c37aa)
+exponent | 83
+
+value    | f64: 0.00000 (1.00000e-310)
+-----------------------
+bits     | 0x12688b70e62b
+sign     | 0
+mantissa | 0.00224712 (0x12688b70e62b)
+exponent | -1022
+
+value    | f64: nan (nan)
+-----------------------
+bits     | 0x7ff0000000000001
+sign     | 0
+mantissa | 1.00000000 (0x000001)
+exponent | 2047 (special)
 ```
--- float.zig
+++ float.zig
@@ -1,53 +1,87 @@
 const std = @import("std");
 const warn = std.debug.warn;
+const IntType = std.meta.IntType;
 
 pub fn main() !void {
-    analyzeFloat(8);
-    analyzeFloat(9.1);
-    analyzeFloat(-120.42);
-    analyzeFloat(@bitCast(f32, @as(u32, 0x7f80_0000))); // inf
+    analyzeFloat(f32, 8);
+    analyzeFloat(f32, 9.1);
+    analyzeFloat(f32, 1.23e-40);
+    analyzeFloat(f32, -120.42);
+    analyzeFloat(f32, @bitCast(f32, @as(u32, 0x7f80_0000))); // inf
+
+    analyzeFloat(f64, 16);
+    analyzeFloat(f64, 12345678900000000000000000.1);
+    analyzeFloat(f64, 1e-310);
+    analyzeFloat(f64, @bitCast(f64, @as(u64, 0x7ff0_0000_0000_0001))); // NaN
 }
 
-const FloatBits = packed struct {
-    mantissa: u23,
-    exponent: u8,
-    sign_bit: u1,
-};
+fn FloatBits(comptime Float: type) type {
+    return switch (Float) {
+        f32 => packed struct {
+            mantissa: u23,
+            exponent: u8,
+            sign_bit: u1,
 
-fn analyzeFloat(f: f32) void {
-    const f_bits = @bitCast(FloatBits, f);
+            const exp_bits = 8;
+            const mantissa_bits = 23;
+        },
+        f64 => packed struct {
+            mantissa: u52,
+            exponent: u11,
+            sign_bit: u1,
 
-    const exp = blk: {
-        const bias = 127;
-        var exp = @intCast(i9, f_bits.exponent);
-        if (exp == 0xff) break :blk exp;
+            const exp_bits = 11;
+            const mantissa_bits = 53;
+        },
+        f128 => @panic("Unimplemented"),
+        else => @panic("Must be float type"),
+    };
+}
+
+fn analyzeFloat(comptime Float: type, f: Float) void {
+    const Bits = FloatBits(Float);
+    const f_bits = @bitCast(Bits, f);
+
+    const is_signed = f_bits.sign_bit == 1;
+
+    // Special value: exponent bits all 1
+    const is_special_value = f_bits.exponent == (1 << Bits.exp_bits) - 1;
+
+    const SignedExp = IntType(true, Bits.exp_bits + 1);
+    const exponent: SignedExp = if (is_special_value)
+        f_bits.exponent
+    else blk: {
+        const bias = (1 << (Bits.exp_bits - 1)) - 1;
+        var exp = @intCast(SignedExp, f_bits.exponent);
         if (exp == 0) exp = 1;
-        break :blk @intCast(i8, exp - bias);
+        break :blk exp - bias;
     };
 
-    const mantissa_f = blk: {
-        var mantissa_f: f64 = if (exp == 0) 0 else 1;
-        var mantissa = f_bits.mantissa;
-        var pow: u64 = 2;
-        while (mantissa != 0) : (mantissa <<= 1) {
-            if (mantissa & 0x0040_0000 != 0)
-                mantissa_f += 1 / @intToFloat(f32, pow);
-            pow *= 2;
-        }
-        if (f_bits.sign_bit == 1) mantissa_f *= -1;
-        break :blk mantissa_f;
+    const mantissa = blk: {
+        var mantissa: Float = if (f_bits.exponent == 0) 0 else 1;
+        mantissa += @intToFloat(Float, f_bits.mantissa) / (1 << Bits.mantissa_bits);
+        break :blk if (is_signed) -mantissa else mantissa;
     };
 
-    warn("value    | {:.4}\n", .{f});
-    warn("-----------------------\n", .{});
-    warn("bits     | 0x{x:0<8}\n", .{@bitCast(u32, f)});
-    warn("sign     | {}\n", .{f_bits.sign_bit});
-    warn("mantissa | {:.5} (0x{x:0<6})\n", .{ mantissa_f, f_bits.mantissa });
+    const fmt =
+        \\value    | {}: {d:.5} ({1:.5})
+        \\-----------------------
+        \\bits     | 0x{x:0<8}
+        \\sign     | {}
+        \\mantissa | {d:.8} (0x{x:0<6})
+        \\exponent | {} {}
+    ++ "\n\n";
 
-    switch (exp) {
-        0x00 => warn("exp      | 0 (denormalized)", .{}),
-        0xff => warn("exp      | 255 (special value)", .{}),
-        else => warn("exp      | {} ({})", .{ exp, f_bits.exponent }),
-    }
-    warn("\n\n", .{});
+    const special_desc = @as([]const u8, if (is_special_value) "(special)" else "");
+    const stdout = std.io.getStdOut();
+    stdout.outStream().print(fmt, .{
+        @typeName(Float),
+        f,
+        @bitCast(IntType(false, @bitSizeOf(Float)), f),
+        f_bits.sign_bit,
+        mantissa,
+        f_bits.mantissa,
+        exponent,
+        special_desc,
+    }) catch unreachable;
 }
